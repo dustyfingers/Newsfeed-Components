4ead4eb140bebdf82dfe8a9cf13f61c362ff27cd
dustyfingers <dustyfingers01257@gmail.com>
2019-06-19 15:59:53 -0600

    finished challenge

--- Article/Article.js
+++ Article/Article.js
@@ -3,21 +3,21 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    this.expandButton.innerText = 'expand';
 
+    this.expandButton.addEventListener('click', () => this.expandArticle());
+  }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
+    this.expandButton.innerText === 'expand' ? this.expandButton.innerText = 'close' : this.expandButton.innerText = 'expand';
   }
 }
-
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
@@ -26,4 +26,6 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(article => new Article(article));
